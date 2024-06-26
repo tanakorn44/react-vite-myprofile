d60d75191fbfa2381f8585e01eac083c8b764d1a
ohmanaza2544@gmail.com <ohmanaza2544@gmail.com>
2024-06-26 22:18:02 +0700

    hometitle เพิ่มstyes  และเนื้อหา

--- src/component/home/Data.jsx
+++ src/component/home/Data.jsx
@@ -4,7 +4,15 @@ const Data = () => {
   return (
     <div className='home__data'>
         <h1 className='home__title'>Tanakorn Chimpalee</h1>
-      
+        <h3>Visual Designer</h3>
+
+        <p className='homee__subtitle'>
+        i'm creative designer based in bangkok ,and i'm passionate and dedicated to my work
+        </p>
+
+        <a href='#contact' className='button button--flex'>
+          Hello
+        </a>
     </div>
   )
 }
--- src/component/home/Social.jsx
+++ src/component/home/Social.jsx
@@ -4,12 +4,12 @@ import React from 'react'
 const Social = () => {
   return (
     <div className='home__social'>
-        <a href="https://www.facebook.com/profile.php?id=100002459808201" className='home__social-icon' target='_blank'></a>
-        <i class="uil uil-facebook"></i>
-        <a href="https://www.instagram.com/tanakorn_ohm/?next=%2F" className='home__social-icon' target='_blank'></a>
-        <i class="uil uil-instagram"></i>
-        <a href="https://github.com/tanakorn44?tab=repositories" className='home__social-icon' target='_blank'></a>
-        <i class="uil uil-github"></i>
+        <a href="https://www.facebook.com/profile.php?id=100002459808201" className='home__social-icon' target='_blank'>
+        <i class="uil uil-facebook"></i></a>
+        <a href="https://www.instagram.com/tanakorn_ohm/?next=%2F" className='home__social-icon' target='_blank'>
+        <i class="uil uil-instagram"></i></a>
+        <a href="https://github.com/tanakorn44?tab=repositories" className='home__social-icon' target='_blank'>
+        <i class="uil uil-github"></i></a>
     </div>
   )
 }
