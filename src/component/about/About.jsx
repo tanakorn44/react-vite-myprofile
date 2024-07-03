1dce39cf9b52e07bf9d6f33dcea0aff4e8dcac12
ohmanaza2544@gmail.com <ohmanaza2544@gmail.com>
2024-07-04 00:40:48 +0700

    jsx,cssหน้า about ทำเสร็จไปแล้ว ตอนนี้กำลังทำหน้า skills //หน้าskills ติดปัญหา Css

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import React from "react";
+import "./about.css";
+import AboutImg from "../../assets/profile.jpg";
+import Info from "./Info";
+import Resume from "../../assets/Resume.pdf"
 
 const About = () => {
   return (
-    <section className='about section' id='about'>
-        <h2 className='section__title'>About Me</h2> 
-        <span className='section__subtitle'>My introduction</span>
-      
-    </section>
-  )
-}
+    <section className="about section" id="about">
+      <h2 className="section__title">About Me</h2>
+      <span className="section__subtitle">My introduction</span>
 
-export default About
+      <div className="about__container container grid">
+        <img src={AboutImg} alt="" className="about__img" />
+
+        <div className="about__data">
+          <Info />
+          <p className="about__description">
+            Frontend developer, I create web pages with UI / UX user interface
+          </p>
+
+          <a download="" href={Resume} className="button button--flex">Download Resume</a>
+        </div>
+
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/component/skills/Skills.jsx
+++ src/component/skills/Skills.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import "./skills.css"
+import Frontend from "./Frontend";
+import Backend from "./Backend";
 
 const Skills = () => {
   return (
-    <section className="" id="">
+    <section className="skills section" id="skills">
       <h2 className="section__title">Skills</h2>
-      <span className='section__subtitle'>My introduction</span>
+      <span className='section__subtitle'>My technical level </span>
+
+      <div className="skills__container container grid">
+        <Frontend />
+        <Backend />
+      </div>
+
     </section>
   );
 };
